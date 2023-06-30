be4d9e21de5d28f03106e8e59f5cbb63adce755c
Natali-Alisa <112685589+Natali-Alisa@users.noreply.github.com>
2023-07-01 00:31:55 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let nameAcademy = "Beetroot"
+console.log(nameAcademy)
+
+let nameCourse = "Front-End"
+const NAMECOUNTRY = "Ukraine"
+let nameStudent = "Natali"
+console.log(nameCourse, NAMECOUNTRY, nameStudent)
+
+let developerSalary = "million"
+console.log(developerSalary)
